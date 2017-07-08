0d482fe6e1c6556600a70ce081cb9b56ce4da9c6
435vic <victor_javier2004@hotmail.com>
2017-07-08 17:39:21 -0500

    Add download link and remove duplicate css entries

--- programs/oop/sketch.js
+++ programs/oop/sketch.js
@@ -1,3 +1,4 @@
+//create new array
 var bubbles = [];
 
 function setup() {
@@ -8,17 +9,20 @@ function setup() {
 
 function draw() {
   background(0);
+  //for each bubble in bubbles, show and move them
   for (var i = 0; i < bubbles.length; i++) {
     bubbles[i].show();
     bubbles[i].move();
   }
 
   if (bubbles.length > 50) {
+    //splice the first (or last drawn bubble) in the array if the array is bigger than 50
     bubbles.splice(0, 1);
   }
 }
 
 
 function mouseDragged () {
+  //push new bubbles to the array when the mouse is dragged
   bubbles.push(new Bubble(mouseX, mouseY));
 }
