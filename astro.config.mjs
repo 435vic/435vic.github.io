2c6df2f569d8c6bde3663f3d2dc51aa06b8cee92
Victor <435victorjavier@gmail.com>
2024-05-16 13:42:52 -0600

    wasm testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
+import topLevelAwait from 'vite-plugin-top-level-await';
+import wasm from 'vite-plugin-wasm';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://435vic.github.io'
+    site: 'https://435vic.github.io',
+    vite: {
+        plugins: [wasm(), topLevelAwait()]
+    }
 });
