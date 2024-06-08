7d14315dda233667cf84ffc80de466215b9a6929
Victor Quintana <435victorjavier@gmail.com>
2024-06-08 15:17:15 -0600

    idk what this did

--- flake.nix
+++ flake.nix
@@ -2,27 +2,42 @@
   description = "boredvico.dev dev - nodejs, rust, wasm-pack support";
 
   inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = {
+    self,
+    nixpkgs,
+    fenix,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        rust-toolchain = with fenix.packages.${system}; [
+          minimal.cargo
+          minimal.rustc
+          targets.wasm32-unknown-unknown.latest.rust-std
+        ];
       in rec {
         devShells.default = pkgs.mkShell {
-	  packages = with pkgs; [
-	    rustc-wasm32
-	    cargo
-	    llvmPackages.bintools
-	    nodejs_22
-	    nodePackages.pnpm
-	    wasm-bindgen-cli
-	  ];
+          packages = with pkgs; [
+            llvmPackages.bintools
+            nodejs_22
+            nodePackages.pnpm
+            wasm-bindgen-cli
+            # ommited for now, as zed doesn't support using rust in a shell environment
+            # such as direnv (might be patched later)
+            # rust-toolchain
+          ];
 
-	  shellHook = ''
-	    export CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER="lld"
-	  '';
+          shellHook = ''
+            export CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER="lld"
+          '';
         };
       }
     );
